5a2352ee0a5df75f40f65b73161f6422005bcb5b
kaikaew13 <ksaratunti@gmail.com>
2021-04-25 17:52:54 +0700

    added login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 const { errorHandler } = require('../helper');
 const User = require('../models/user');
@@ -25,6 +26,35 @@ exports.signup = async (req, res, next) => {
     const message = 'signed up successfully';
     console.log(message);
     res.status(201).json({ message: message });
+  } catch (err) {
+    errorHandler(err, next);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const err = new Error('no user with this email');
+      err.statusCode = 401;
+      throw err;
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      const err = new Error('wrong password');
+      err.statusCode = 401;
+      throw err;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+      },
+      'secret-my-kitsu',
+      { expiresIn: '1h' }
+    );
+    const message = 'logged in successfully';
+    res.status(200).json({ token: token, message: message });
   } catch (err) {
     errorHandler(err, next);
   }
--- routes/auth.js
+++ routes/auth.js
@@ -15,4 +15,6 @@ router.post(
   authController.signup
 );
 
+router.post('/login', authController.login);
+
 module.exports = router;
