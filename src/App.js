ab0b1aec77b494828e02feba07b1c9b8fcf657b7
kaikaew13 <ksaratunti@gmail.com>
2021-04-30 21:24:45 +0700

    added modal container and toggle modal to go from on to off

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NavContainer from './containers/navigation/nav-container';
 import SectionContainer from './containers/section/section-container';
 import GenreContainer from './containers/genre/genre-container';
+import Modal from './containers/modal/modal';
+import Backdrop from './components/UI/backdrop';
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+  const toggleModal = () => {
+    setShowModal((prevState) => !prevState);
+  };
   return (
     <div className="App">
+      {showModal ? (
+        <React.Fragment>
+          <Modal />
+          <Backdrop class="backdrop-dark" clicked={toggleModal} />
+        </React.Fragment>
+      ) : null}
+
       <NavContainer />
       <SectionContainer />
       <GenreContainer />
---
+++ src/containers/modal/modal.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Modal = (props) => <div className="modal"></div>;
+
+export default Modal;
