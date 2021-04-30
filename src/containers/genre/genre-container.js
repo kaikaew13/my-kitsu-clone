17dff0b2959c379b439f081417fbb74ddb549b85
kaikaew13 <ksaratunti@gmail.com>
2021-04-30 11:15:09 +0700

    populated the div with genres/ genre section completed

--- src/containers/genre/genre-container.js
+++ src/containers/genre/genre-container.js
@@ -3,9 +3,28 @@ import React from 'react';
 import GenreList from './genre-list';
 
 const GenreContainer = (props) => (
-  <div class="genre-container">
-    <GenreList class="genre-fav-categories">my favourite categories</GenreList>
-    <GenreList class="genre-categories">categories</GenreList>
+  <div className="genre-container">
+    <GenreList favourite={true}>my favourite categories</GenreList>
+    <GenreList
+      favourite={false}
+      genreList={[
+        'Action',
+        'Adventure',
+        'Comedy',
+        'Drama',
+        'Slice of Life',
+        'Fantasy',
+        'Magic',
+        'Supernatural',
+        'Horror',
+        'Mystery',
+        'Psychological',
+        'Romance',
+        'Sci-Fi',
+      ]}
+    >
+      categories
+    </GenreList>
   </div>
 );
 
--- src/containers/genre/genre-list.js
+++ src/containers/genre/genre-list.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
-const GenreList = (props) => (
-  <div className={props.class}>
-    <h5 className="categories-head">{props.children.toUpperCase()}</h5>
-    <ul className="genre-list"></ul>
-  </div>
-);
+import EachGenre from '../../components/each-genre/each-genre';
+
+const GenreList = (props) =>
+  props.favourite ? (
+    <div className="genre-fav-categories">
+      <h5 className="categories-head">{props.children.toUpperCase()}</h5>
+      <ul class="genre-list">
+        Favoriting categories will improve your recommendations.
+      </ul>
+    </div>
+  ) : (
+    <div className="genre-categories">
+      <h5 className="categories-head">{props.children.toUpperCase()}</h5>
+      <ul className="genre-list">
+        {props.genreList.map((each, index) => (
+          <EachGenre key={index}>{each}</EachGenre>
+        ))}
+      </ul>
+      <div className="section-view-more">
+        <a href="/">more categories...</a>
+      </div>
+    </div>
+  );
 
 export default GenreList;
