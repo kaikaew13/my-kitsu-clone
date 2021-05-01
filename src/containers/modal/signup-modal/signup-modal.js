dc7f7c9e3eb621e3e05c249b407b36c26a3ff422
kaikaew13 <ksaratunti@gmail.com>
2021-05-01 11:46:49 +0700

    completed length validation for username and password

--- src/containers/modal/signup-modal/signup-modal.js
+++ src/containers/modal/signup-modal/signup-modal.js
@@ -16,7 +16,7 @@ const SignupModal = (props) => {
     touched: false,
     validation: { minLength: 5, pass: false },
   });
-  console.log(password.validation.pass);
+
   return (
     <form className="modal-signup">
       <div className="modal-signup-items">
@@ -30,16 +30,37 @@ const SignupModal = (props) => {
           value={username.val}
           onChange={(e) => {
             let pass = false;
+            let touched = true;
             if (e.target.value.length >= username.validation.minLength)
               pass = true;
+            if (e.target.value.length === 0) touched = false;
             setUsername((prevState) => ({
               ...prevState,
-              touched: true,
+              touched: touched,
               val: e.target.value,
               validation: { ...prevState.validation, pass: pass },
             }));
           }}
+          style={
+            username.touched && !username.validation.pass
+              ? { borderBottom: '1px solid rgb(240, 173, 78)' }
+              : username.validation.pass && username.touched
+              ? { borderBottom: '1px solid rgb(92, 184, 92)' }
+              : null
+          }
         />
+        {username.touched && !username.validation.pass && (
+          <div
+            style={{
+              color: 'rgb(240, 173, 78)',
+              fontFamily: 'sans-serif',
+              fontSize: '16px',
+            }}
+          >
+            This field is too short (minimum is {username.validation.minLength}{' '}
+            characters)
+          </div>
+        )}
       </div>
       <div className="modal-signup-items">
         <h6>What email should we use?</h6>
@@ -71,16 +92,37 @@ const SignupModal = (props) => {
           value={password.val}
           onChange={(e) => {
             let pass = false;
+            let touched = true;
             if (e.target.value.length >= password.validation.minLength)
               pass = true;
+            if (e.target.value.length === 0) touched = false;
             setPassword((prevState) => ({
               ...prevState,
-              touched: true,
+              touched: touched,
               val: e.target.value,
               validation: { ...prevState.validation, pass: pass },
             }));
           }}
+          style={
+            password.touched && !password.validation.pass
+              ? { borderBottom: '1px solid rgb(240, 173, 78)' }
+              : password.validation.pass && password.touched
+              ? { borderBottom: '1px solid rgb(92, 184, 92)' }
+              : null
+          }
         />
+        {password.touched && !password.validation.pass && (
+          <div
+            style={{
+              color: 'rgb(240, 173, 78)',
+              fontFamily: 'sans-serif',
+              fontSize: '16px',
+            }}
+          >
+            This field is too short (minimum is {password.validation.minLength}{' '}
+            characters)
+          </div>
+        )}
       </div>
       <button
         type="submit"
