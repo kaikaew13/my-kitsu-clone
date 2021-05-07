9cae0e9e24f6c4d723c1f3282c33da5e59cff0d1
kaikaew13 <ksaratunti@gmail.com>
2021-05-07 18:04:12 +0700

    added getEachReaction endpoint

--- controllers/website.js
+++ controllers/website.js
@@ -1,6 +1,6 @@
 const { errorHandler } = require('../helper');
 const Anime = require('../models/anime');
-const User = require('../models/user');
+const Reaction = require('../models/reaction');
 
 exports.getHome = async (req, res, next) => {
   let limit = req.params.limit === 'no-limit' ? null : +req.params.limit;
@@ -28,6 +28,25 @@ exports.getEachAnime = async (req, res, next) => {
     }
     const message = 'fetched anime from db successfully';
     res.status(200).json({ message: message, anime: anime });
+  } catch (err) {
+    errorHandler(err, next);
+  }
+};
+
+exports.getEachReaction = async (req, res, next) => {
+  const animeId = req.params.animeId;
+  try {
+    const anime = await Anime.findById(animeId);
+    if (!anime) {
+      const err = new Error('not a valid url');
+      err.statusCode(404);
+      throw err;
+    }
+    const reactions = await Reaction.find({
+      animeId: animeId,
+    }).populate('userId', ['username', '_id']);
+    const message = 'fetched reaction successfully';
+    res.status(200).json({ message: message, reactions: reactions });
   } catch (err) {
     errorHandler(err, next);
   }
